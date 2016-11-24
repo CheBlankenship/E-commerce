4509b8e3393a6c1e6a97c1a181bf0e38609a9636
Che B <cheb@Ches-MacBook-Air.local>
2016-11-23 22:33:48 -0500

    update server.py

---
+++ example.py
@@ -0,0 +1,139 @@
+from flask import Flask, jsonify, request, redirect
+import bcrypt, uuid, pg, os
+
+
+db = pg.DB(dbname='ecommerce_db')
+app = Flask('ecommerce', static_url_path="")
+
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/products')
+def products():
+    product_list = db.query('SELECT * FROM product').dictresult()
+    return jsonify(product_list)
+
+
+@app.route('/api/products/<prod_id>')
+def products_details(prod_id):
+    product_details = db.query('SELECT * FROM product WHERE product.id = $1', prod_id).dictresult()
+    return jsonify(product_details)
+
+
+@app.route('/api/user/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    password = data['password'] # the entered password
+    salt = bcrypt.gensalt() # generate a salt
+    # generate the encrypted password
+    encrypted_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return jsonify(db.insert('customer', {
+        'username': data['username'],
+        'email': data['email'],
+        'first_name': data['first_name'],
+        'last_name': data['last_name'],
+        'password': encrypted_password
+    }))
+
+
+@app.route('/api/user/login', methods=['POST'])
+def login():
+    user = request.get_json()
+    username = user['username']
+    password = user['password']
+    salt = bcrypt.gensalt() # generate a salt
+    encrypted_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    rehash = bcrypt.hashpw(password.encode('utf-8'), encrypted_password)
+    print encrypted_password, rehash
+    if rehash == encrypted_password:
+        token = uuid.uuid4()
+        user = db.query('SELECT id, first_name FROM customer WHERE username = $1', username).namedresult()[0]
+        db.insert (
+            "auth_token",
+            token = token,
+            customer_id = user.id
+        )
+        login_data = {
+            'token': token,
+            'customer_id': user.id,
+            'user_name' : user.first_name
+        }
+        return jsonify(login_data)
+    else:
+        return "Incorrect password", 401
+
+
+@app.route('/api/shopping_cart', methods=['POST'])
+def add_product_to_cart():
+    data = request.get_json()
+    sent_token = data.get('token')
+    product_id = data.get('product_id')
+    customer = db.query('SELECT * FROM auth_token WHERE token = $1 AND now() < token_expires', sent_token).namedresult()
+    if customer == []:
+        return "Forbidden", 403
+    else:
+        customer_id = customer[0].customer_id
+        customer_token = customer[0].token
+        db.insert('product_in_shopping_cart', {
+            'product_id' : product_id,
+            'customer_id' : customer_id
+        })
+        return jsonify(customer)
+
+
+@app.route('/api/shopping_cart')
+def view_cart():
+    sent_token = request.args.get('token')
+    customer = db.query('SELECT * FROM auth_token WHERE token = $1 AND now() < token_expires', sent_token).namedresult()
+    if customer == []:
+        return "Forbidden", 403
+    else:
+        results = db.query('''
+        SELECT product.name
+            FROM product_in_shopping_cart
+            INNER JOIN product ON product.id = product_id
+            INNER JOIN auth_token ON auth_token.customer_id = product_in_shopping_cart.customer_id
+            WHERE auth_token.token = $1''', sent_token).namedresult()
+        return jsonify(results)
+
+
+@app.route('/api/shopping_cart/checkout', methods=['POST'])
+def checkout():
+    data = request.get_json()
+    sent_token = data.get('token')
+    customer = db.query('SELECT * FROM auth_token WHERE token = $1 AND now() < token_expires', sent_token).namedresult()
+    if customer == []:
+        return "Forbidden", 403
+    else:
+        customer_id = customer[0].customer_id
+        customer_token = customer[0].token
+        total_price = db.query("""
+        SELECT sum(price)
+            FROM product_in_shopping_cart
+            INNER JOIN product ON product.id = product_id
+            INNER JOIN auth_token ON auth_token.customer_id = product_in_shopping_cart.customer_id
+            WHERE auth_token.token = $1""", customer_token).namedresult()[0].sum
+        purchased_items = db.query("""
+        SELECT price, product.name, product.id
+            FROM product_in_shopping_cart
+            INNER JOIN product ON product.id = product_id
+            INNER JOIN auth_token ON auth_token.customer_id = product_in_shopping_cart.customer_id
+            WHERE auth_token.token = $1""", customer_token).dictresult()
+        purchase = db.insert('purchase', {
+            'customer_id': customer_id,
+            'total_price': total_price
+        })
+        for item in purchased_items:
+            db.insert('product_in_purchase', {
+                'product_id': item['id'],
+                'purchase_id': purchase['id']
+            })
+        db.query('DELETE FROM product_in_shopping_cart WHERE customer_id = $1', customer_id)
+        return jsonify(purchase)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- server.py
+++ server.py
@@ -1,83 +1,130 @@
 from flask import Flask, jsonify, request
 import pg
-
 import bcrypt
-
-password = 'opensesame' # the entered password
-salt = bcrypt.gensalt() # generate a salt
-# now generate the encrypted password
-encrypted_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-
 import uuid
 
-db = pg.DB(dbname = "e-commerce")
+
+db = pg.DB(dbname ='e-commerce')
 app = Flask('eCommerce')
 
 
+# List all the products
 @app.route('/api/products', methods=['GET'])
 def listAllProducts():
-   query = db.query("select * from product").namedresult()
-   return jsonify(query)
+    all_products = db.query("select * from product").namedresult()
+    return jsonify(all_products)
 
 
-@app.route('/api/product/<id>', methods=['GET'])
-def individualProduct(id):
-    query = db.query("select * from product where id = %s" % id).namedresult()
-    result = query[0].name
-    return jsonify(result)
+# Show product based on ID
+@app.route('/api/product/<product_id>', methods=['GET'])
+def individualProduct(product_id):
+    specifice_product= db.query("select * from product where id = $1", product_id).namedresult()
+    return jsonify(specifice_product)
 
 
+# User sign up page
 @app.route('/api/user/signup', methods=['POST'])
 def user_signup():
     data = request.get_json()
     password = data['password']
     salt = bcrypt.gensalt()
     encrypted_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    db.insert(
+    return jsonify(db.insert(
         'customer',
-        username = data['username'],
-        password = encrypted_password,
-        email = data['email'],
-        first_name = data['first_name'],
-        last_name = data['last_name']
-    )
+        {
+            'username' : data['username'],
+            'password' : encrypted_password,
+            'email' : data['email'],
+            'first_name' : data['first_name'],
+            'last_name' : data['last_name']
+        }
+    ))
 
-
-@app.route('/api/user/login', methods=['POST'])
+# User login page
+@app.route('/api/user/login', methods = ['POST'])
 def user_login():
-    data = request.get_json()
-    # request.get_json().get('auth_token')
-    query = db.query("select password, id from customer where username = $1" ,data['username']).namedresult()
-
-    password = data["password"]
-
-    encrypted_password = query[0].password  # encrypted password retrieved
-    # from database record
-    # the following line will take the original salt that was used
-    # in the generation of the encrypted password, which is stored as
-    # part of the encrypted_password, and hash it with the entered password
+    user = request.get_json()
+    username = user['username']
+    password = user['password']
+    salt = bcrypt.gensalt()
+    encrypted_password = bcrypt.hashpw(password.encode('utf-8'),salt)
     rehash = bcrypt.hashpw(password.encode('utf-8'), encrypted_password)
-    # if we get the same result, that means the password was correct
-    customer_id = query[0].id
-
-    db.insert(
-        'auth_token',
-        token = uuid.uuid4(),
-        customer_id = customer_id
-    )
 
     if rehash == encrypted_password:
-        success = "success"
+        token = uuid.uuid4()
+        user = db.query("select id, first_name from customer where username = $1", username).namedresult()[0]
+        db.insert(
+            "auth_token",
+            token = token,
+            customer_id = user.id
+        )
+        login_data = {
+        'token': token,
+        'customer_id': user.id,
+        'user_name': user.first_name
+        }
+        return jsonify(login_data)
     else:
-        success = "failed"
+        return "Login failed", 401
 
-    return success
+# Add products to cart by using token and pro_id
+@app.route('/api/shopping_cart', methods = ['POST'])
+def add_product_to_cart():
+    data = request.get_json()
+    print "check"
+    print data
+    auth_token = data.get('token')
+    # print 'AUTH TOKEN %s', auth_token
+    product_id = data.get('product_id')
+    customer = db.query("select * from auth_token where token = $1", auth_token).namedresult()[0]
+    if customer == []:
+        return "FAIL"
+        # return "Forbidden", 403
+    else:
+        customer_id = customer.customer_id
+        customer_token = customer.token
+        db.insert(
+            'product_in_shopping_cart',{
+                'product_id': product_id,
+                'customer_id': customer_id
+            }
+        )
+        return jsonify(customer)
 
 
+@app.route('/api/shopping_cart', methods = ['GET'])
+def view_cart():
+    token = "e4588540-b87f-4604-af6d-1046b8c8bdc9"
+
+    # save for later when merging front end has started
+    # sent_token = request.args.get('token')
+    customer = db.query("select * from auth_token where token = $1", token).namedresult()
+    if customer == []:
+        return "Failed", 403
+    else:
+        inTheCart = db.query("select product.name, product.price from product_in_shopping_cart inner join product on product.id = product_in_shopping_cart.product_id inner join auth_token on product_in_shopping_cart.customer_id = auth_token.customer_id where auth_token.token = $1", token).namedresult()
+        return jsonify(inTheCart)
 
 
-
+@app.route('/api/shopping_cart/checkout', methods=['POST'])
+def checkout():
+    data = request.get_json()
+    token = data.get('token')
+    customer = db.query("select * from auth_token where token = $1", token).namedresult()
+    if customer == []:
+        return 'No thing info'
+    else:
+        customer_id = customer[0].customer_id
+        customer_token = customer[0].token
+        total_price = db.query(""" SELECT sum(price)
+                                        from product_in_shopping_cart
+                                        inner join product on product.id = product_in_shopping_cart.product_id
+                                        inner join auth_token on auth_token.customer_id = product_in_shopping_cart.customer_id where auth_token.token = $1""", customer_token).namedresult()[0].sum
+        return jsonify(db.insert('purchase',{
+                'customer_id' : customer_id,
+                'total_price' : total_price
+            }))
 
 
 if __name__ == '__main__':
-   app.run(debug=True);
+    app.run(debug=True)
